5d018e9974e19e7fb838e30dfd978c11e6f5d09f
RubenSugianto <ruben.sugianto@gmail.com>
2025-05-13 23:10:57 +0700

    Grouping Compound Components

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <section>
       <h2>Why work with us?</h2>
       <Accordion className="accordion">
-        <AccordionItem id="experience" className="accordion-item" title="We got 20 years of experience">
+        <Accordion.Item id="experience" className="accordion-item" title="We got 20 years of experience">
           <article>
             <p>You can&apos;t go wrong with us.</p>
             <p>
@@ -14,15 +14,15 @@ function App() {
               vacation trips for more than 20 years.
             </p>
           </article>
-        </AccordionItem>
-        <AccordionItem  id="local-guides" className="accordion-item"  title="We're working with local guides">
+        </Accordion.Item>
+        <Accordion.Item  id="local-guides" className="accordion-item"  title="We're working with local guides">
           <article>
             <p>We are not doing this along from our office.</p>
             <p>
               Instead, we are working with local guides to ensure a safe and pleasant vacation.
             </p>
           </article>
-        </AccordionItem>
+        </Accordion.Item>
       </Accordion>
     </section>
   </main>
